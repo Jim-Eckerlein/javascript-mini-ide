c03f547e09767ca664692822060a8439f1a50b78
Jim-Eckerlein <jim.eckerlein@gmail.com>
2017-10-06 17:12:33 +0200

    Move on key tap cancels

--- app/src/main/java/com/example/jimec/javascriptshell/keyboard_view/TapListener.java
+++ app/src/main/java/com/example/jimec/javascriptshell/keyboard_view/TapListener.java
@@ -16,10 +16,16 @@ public abstract class TapListener implements View.OnTouchListener {
     // Time of a continuous tap interpreted as a long tap
     private static final long LONG_TOUCH_MILLIS = 150;
     
+    private static final int MAX_DISTANCE = 50;
+    
     // Becomes true as soon as tap is recognized as long tap and therefore processed already
     private boolean mLongTapped = false;
     
     private Timer mLongTapTimer;
+    
+    private float mStartX;
+    private float mStartY;
+    private boolean mExceededDistance = false;
     
     @Override
     public boolean onTouch(View view, MotionEvent event) {
@@ -30,11 +36,22 @@ public abstract class TapListener implements View.OnTouchListener {
             mLongTapped = false;
             mLongTapTimer = new Timer();
             mLongTapTimer.schedule(new LongTapTimer(), LONG_TOUCH_MILLIS);
+            mStartX = event.getX();
+            mStartY = event.getY();
+            mExceededDistance = false;
             onDown();
             processed = true;
         }
-    
-        else if (MotionEvent.ACTION_UP == action) {
+
+        else if (MotionEvent.ACTION_MOVE == action && !mExceededDistance) {
+            if (Math.hypot(event.getX() - mStartX, event.getY() - mStartY) > MAX_DISTANCE) {
+                // Touch move exceeded distance
+                mExceededDistance = true;
+                mLongTapTimer.cancel();
+            }
+        }
+
+        else if (MotionEvent.ACTION_UP == action && !mExceededDistance) {
             mLongTapTimer.cancel();
             if (!mLongTapped) {
                 // Initiate short touch event
