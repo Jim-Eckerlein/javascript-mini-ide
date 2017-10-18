bae60dc02761d89b82ab75cfec943e6bcce909c9
Jim-Eckerlein <jim.eckerlein@gmail.com>
2017-10-18 23:17:04 +0200

    Add regex example

---
+++ app/src/main/res/raw/example_regex.js
@@ -0,0 +1,10 @@
+
+let text = "This is a long text"
+let regex = /long/i;
+
+let position = text.search(regex)
+print(`Position of ${/long/i} within "${text}": ${position}\n`)
+
+// Replace 'long' by 'short'
+text = text.replace(regex, 'short')
+print(text)
